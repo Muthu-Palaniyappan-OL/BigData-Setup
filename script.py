b11fa51ba624cbd36deaaee4794516e2401061c4
Muthu Palaniyappan OL <muthu892542@gmail.com>
2022-11-27 23:43:35 +0530

    Update script.py

--- script.py
+++ script.py
@@ -2,7 +2,11 @@ import requests
 import time
 import os
 
+const topics = ['BigData', 'DataScience', 'CEG']
+count = 0
+
 while True:
 	time.sleep(3)
-	r = requests.get("https://api.twitter.com/2/tweets/search/recent?query=BigData&max_results=100", headers={'Authorization': 'Bearer ' + os.environ.get('BEARER_TOKEN')})
+	r = requests.get("https://api.twitter.com/2/tweets/search/recent?query="+topics[count % len(topics)]+"&max_results=20", headers={'Authorization': 'Bearer ' + os.environ.get('BEARER_TOKEN')})
 	print(r.text)
+	count += 1
